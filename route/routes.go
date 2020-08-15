1c0c6b3cf8e6288cbdb4e5becd1db92fb11ba924
Adev <adevsaputra3@gmail.com>
2020-08-15 22:35:54 +0700

    add 2 routes post and get

--- route/routes.go
+++ route/routes.go
@@ -17,6 +17,8 @@ func SetupRouter() *gin.Engine {
 		{
 			v1g.Use(middlewares.JSONOnly)
 			v1g.POST("/user", v1.InsertUser)
+			v1g.POST("/com/request", v1.PlaceRequest)
+			v1g.GET("/gen/marketplace", v1.ProductRequestList)
 		}
 	}
 	return router
