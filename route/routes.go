355269737cd9aec523d8bdeddcb9936267fca789
Adev <adevsaputra3@gmail.com>
2020-08-16 00:15:07 +0700

    add requestlist route

--- route/routes.go
+++ route/routes.go
@@ -18,7 +18,9 @@ func SetupRouter() *gin.Engine {
 			v1g.POST("/signup", v1.InsertUser)
 			v1g.POST("/signin", v1.FindUser)
 			v1g.POST("/com/request", v1.PlaceRequest)
+			v1g.POST("/com/requests", v1.RequestList)
 			v1g.GET("/gen/marketplace", v1.ProductRequestList)
+
 		}
 	}
 	return router
