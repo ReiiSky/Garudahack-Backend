4c2b1bd2f33f09e92c72ac9d263f10ca2bd3b806
Entalizen <danielarya185@gmail.com>
2020-08-15 22:33:51 +0900

    refactor: remove validation because i don't need it

--- services/api/v1/user.go
+++ services/api/v1/user.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/Kamva/mgm/v3"
 	"github.com/Satssuki/Go-Service-Boilerplate/models"
-	"github.com/Satssuki/Go-Service-Boilerplate/services/api/validation"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
@@ -21,22 +20,18 @@ func CreateUserService() UserService {
 
 // Insert implementation of function in base interface
 func (user *UserService) Insert() (string, error) {
-	err := validation.ValidateUser(&user.User)
 	var message string = "Users created"
-	if err == nil {
-		_ = err
-		currentUser := &user.User
-		count, Err := currentUser.GetCollection().CountDocuments(mgm.Ctx(), bson.M{
-			"name":  user.User.Name,
-			"email": user.User.Email,
-		})
-		err = Err
-		if count > 0 {
-			message = "Users already defined"
-			err = errors.New(message)
-		} else {
-			err = currentUser.GetCollection().Create(currentUser)
-		}
+	currentUser := &user.User
+	count, err := currentUser.GetCollection().CountDocuments(mgm.Ctx(), bson.M{
+		"name":  user.User.Name,
+		"email": user.User.Email,
+	})
+	
+	if count > 0 {
+		message = "Users already defined"
+		err = errors.New(message)
+	} else {
+		err = currentUser.GetCollection().Create(currentUser)
 	}
 	return message, err
 }
